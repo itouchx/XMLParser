75d08ff53f649f2c983fc79f50ce6508f0e8cd2f
frank <itouch@188.com>
2015-11-18 23:24:17 +0800

    create
    Signed-off-by: frank <itouch@188.com>

---
+++ XMLParser/XMLParser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLParser
+//
+//  Created by WG on 15/11/18.
+//  Copyright © 2015年 WG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMLParser/XMLParser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMLParser
+//
+//  Created by WG on 15/11/18.
+//  Copyright © 2015年 WG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
