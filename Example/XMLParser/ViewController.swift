fca5b36494ed9e7c35e175be47441bd9d6b97fb8
WG <frank.wang@ubtrobot.com>
2017-08-06 18:42:53 +0800

    OC->Swift

---
+++ Example/XMLParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLParser
+//
+//  Created by Laughing(itouch@188.com) on 2017/8/5.
+//  Copyright © 2017年 WG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
